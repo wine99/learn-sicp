e1bd0e3e272f79c963475f84b1eb8adaf3c511ac
Zijun Yu <yu1319324245@gmail.com>
2020-06-21 20:10:31 +0800

    chap1 ex1.3

---
+++ chap1/0.rkt
@@ -0,0 +1,31 @@
+#lang sicp
+
+(define (try guess x)
+  (if (good-enough? guess x)
+      guess
+      (try (improve guess x) x)))
+
+(define (good-enough? guess x)
+  (< (abs (- guess (/ x guess))) 0.0001))
+
+(define (improve guess x)
+  (/ (+ guess (/ x guess)) 2))
+
+
+(define (sqrt x)
+  (try 1 x))
+
+
+(define (another-sqrt x)
+  (define (try guess)
+    (if (good-enough? guess)
+        guess
+        (try (improve guess))))
+  
+  (define (good-enough? guess)
+    (< (abs (- guess (/ x guess))) 0.0001))
+  
+  (define (improve guess)
+    (/ (+ guess (/ x guess)) 2))
+
+  (try 1))
